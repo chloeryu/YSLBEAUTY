f0936090eddf52067335a7656ec6deaf65acf620
chloeryu <yeonhee8012@gamil.com>
2023-06-19 13:32:06 +0900

    list

---
+++ app/list/page.js
@@ -0,0 +1,14 @@
+
+export default function List() {
+  return (
+    <div>
+      <h2>베스트셀러</h2>
+      <div className="lip">
+        <h4>베르니 아 레브르 바이닐 크림틴트 49.000</h4>
+      </div>
+      <div className="lip">
+        <h4>캔디 글레아즈컬러밤 49.000</h4>
+      </div>
+    </div>
+  )
+}
