a8a221fe4a5881c90384e8110a8b0d8b31f943fa
chloeryu <yeonhee8012@gmail.com>
2023-06-20 14:45:00 +0900

    .

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,18 @@
 
 export default function List() {
+  let 상품 = ['베르니 아 레브르 바이닐 크림틴트', '캔디 글레아즈컬러밤']
+
+  let a = [20, 30]
+  console.log(a)
+
   return (
     <div>
       <h4 className="title">베스트셀러</h4>
       <div className="lip">
-        <h4>베르니 아 레브르 바이닐 크림틴트 49.000</h4>
+        <h4>상품1 49.000</h4>
       </div>
       <div className="lip">
-        <h4>캔디 글레아즈컬러밤 49.000</h4>
+        <h4>상품2 49.000</h4>
       </div>
     </div>
   )
